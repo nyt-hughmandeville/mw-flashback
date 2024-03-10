9b3d3cb2bac633314ee3613171b18c4429d4f5b1
Hubert Mandeville <hubert.mandeville@nytimes.com>
2024-03-10 17:34:54 -0400

    added drop

--- client/app/page.js
+++ client/app/page.js
@@ -6,7 +6,14 @@ import styles from "./page.module.css";
 export default function Home() {
   const events = data.map((event) => {
     return (
-      <div key={event.id} className={styles.box_event} draggable="true">
+      <div
+        key={event.id}
+        className={styles.box_event}
+        draggable="true"
+        onDragStart={(e) => {
+          e.dataTransfer.setData("text/plain", event.id);
+        }}
+      >
         <img src={event.image} alt="" />
         <div className={styles.description}>{event.description}</div>
       </div>
@@ -16,10 +23,22 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.box_events}>
-        <div className={styles.lable}>BEFORE</div>
+        <div className={styles.label}>BEFORE</div>
         {events}
-        <div droppable="true" className={styles.box_answers}></div>
-        <div className={styles.lable}>AFTER</div>
+        <div
+          onDrop={(e) => {
+            e.preventDefault();
+            console.log("Dropped: ", e.dataTransfer.getData("text/plain"));
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+            //console.log("Dragged over: ", e);
+          }}
+          className={styles.box_answers}
+        >
+          TBD
+        </div>
+        <div className={styles.label}>AFTER</div>
       </div>
     </main>
   );
