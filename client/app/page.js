777aa023898c219b95334e2f0aa6102abe1317aa
Hubert Mandeville <hubert.mandeville@nytimes.com>
2024-03-17 13:24:20 -0400

    made timeline component

--- client/app/page.js
+++ client/app/page.js
@@ -2,6 +2,7 @@
 
 import data from "../data/nyt-2024-03-01.json";
 import ProgressBar from "./components/progress-bar";
+import Timeline from "./components/timeline";
 import styles from "./page.module.css";
 import {useState} from "react";
 
@@ -9,45 +10,10 @@ export default function Home() {
   const [index, setIndex] = useState(1);
   const [events, setEvents] = useState(data);
 
-  const timeline = data.map((event) => {
-    return (
-      <div
-        key={event.id}
-        className={styles.box_event}
-        draggable="true"
-        onDragStart={(e) => {
-          e.dataTransfer.setData("text/plain", event.id);
-        }}
-      >
-        <div className={styles.year}>{event.displayYear}</div>
-        <div className={styles.event_data}>
-          <img src={event.image} alt="" />
-          <div className={styles.description}>{event.description}</div>
-        </div>
-      </div>
-    );
-  });
-
   return (
     <main className={styles.main}>
       <ProgressBar events={events} index={index} />
-      <div className={styles.box_events}>
-        <div className={styles.label}>BEFORE</div>
-        {timeline}
-        <div
-          onDrop={(e) => {
-            e.preventDefault();
-            console.log("Dropped: ", e.dataTransfer.getData("text/plain"));
-          }}
-          onDragOver={(e) => {
-            e.preventDefault();
-          }}
-          className={styles.box_answers}
-        >
-          TBD
-        </div>
-        <div className={styles.label}>AFTER</div>
-      </div>
+      <Timeline events={events} index={index} />
     </main>
   );
 }
