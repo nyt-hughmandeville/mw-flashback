44891773652bf813e981bcdf6bbde143afd7d9aa
Hubert Mandeville <hubert.mandeville@nytimes.com>
2024-03-22 15:14:25 -0400

    Added guessing component

--- client/app/page.js
+++ client/app/page.js
@@ -1,19 +1,20 @@
 "use client";
 
 import data from "../data/nyt-2024-03-01.json";
+import Guessing from "./components/guessing";
 import ProgressBar from "./components/progress-bar";
 import Timeline from "./components/timeline";
 import styles from "./page.module.css";
 import {useState} from "react";
 
 export default function Home() {
-  const [index, setIndex] = useState(9);
+  const [index, setIndex] = useState(0);
   const [events, setEvents] = useState(data);
 
   return (
     <main className={styles.main}>
       <ProgressBar events={events} index={index} />
-      <hr />
+      <Guessing events={events} index={index} />
       <Timeline events={events} index={index} />
     </main>
   );
