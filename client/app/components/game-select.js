6f7a673e4684f09b319058e78afbd5c430a0298d
Hubert Mandeville <hubert.mandeville@nytimes.com>
2024-05-18 19:06:48 -0400

    Added

---
+++ client/app/components/game-select.js
@@ -0,0 +1,25 @@
+import styles from "./game-select.module.css";
+import Select from "react-select";
+
+// GameSelect - Render game selection dropdown.
+export default function GameSelect({game, games, setGame}) {
+  // Shared style for all select lists.
+  const selectStyles = {};
+
+  return (
+    <div className={styles.game_select}>
+      <Select
+        instanceId="game_list"
+        isClearable={false}
+        isMulti={false}
+        isSearchable={true}
+        name="game"
+        onChange={setGame}
+        options={games}
+        placeholder="Select a game"
+        styles={selectStyles}
+        value={game}
+      />
+    </div>
+  );
+}
